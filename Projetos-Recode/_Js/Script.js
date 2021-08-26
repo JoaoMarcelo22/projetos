72fdaf243897d523ec907fee0e28012df7529098
João Marcelo <joao_marcelo.22@hotmail.com>
2021-08-26 10:46:34 -0300

    update

--- Projetos-Recode/_Js/Script.js
+++ Projetos-Recode/_Js/Script.js
@@ -16,3 +16,10 @@ let exibir_todos =() =>{
 
     } 
 }
+let destaque =(imagem) => {
+    console.log(imagem);
+    if(imagem.width==240)
+    imagem.width=120;
+    else
+    imagem.width = 240;
+}
