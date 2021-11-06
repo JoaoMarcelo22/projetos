dcc00b84d3ae9cfd4fa10b1a08034e9aa8c4927c
João Marcelo <joao_marcelo.22@hotmail.com>
2021-11-06 00:19:44 -0300

    javaScript

---
+++ Hortifruti/codigo.js
@@ -0,0 +1,9 @@
+let data = new Date();
+let mes = data.getMonth();
+
+if (mes=10){
+	saudacao = ('Novembro')
+} else{
+	saudacao = ('Vc está fodido meu parça')
+}
+console.log('Seu mês é ' + saudacao)
